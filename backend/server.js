4b515d1605ae6574ec0599d4e2b7889ac02fa43c
Eduardo <hurtado.josegomez@gmail.com>
2024-01-26 21:17:12 -0600

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,11 @@ const app = express()
 const path = require('path');
 const port = 3000 // npm run dev
 
+app.use(express.static('./backend')); // Ahorita está como ./src pero para tener todo el acceso al servidor deber ser './'
+app.use('/backend', express.static('backend'))
+
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/index.html'));
 })
 
 app.listen(port, () => {
