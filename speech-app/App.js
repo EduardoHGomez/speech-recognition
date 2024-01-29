97dfae65cbb8c423fc5483300ed80a584bb2e6a9
Eduardo <hurtado.josegomez@gmail.com>
2024-01-28 20:06:22 -0600

    expo app

--- speech-app/App.js
+++ speech-app/App.js
@@ -1,13 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [count, setCount] = useState(0);
+
+	const updateCount = () => {
+		setCount(count + 1);
+		console.log('here');
+	}
+
+	return (
+		<View style={styles.container}>
+				<Text>Welcome to Edu App!</Text>
+				<Button
+					onClick={updateCount}
+					title='Press me update count'
+					>
+				</Button>	
+				<Text>Current count: {count}</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
