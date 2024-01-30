0af722afd404dd58cee43ccb284e7c552f18a22f
Eduardo <hurtado.josegomez@gmail.com>
2024-01-30 13:25:38 -0600

    activity ui:

--- speech-app/components/Activities.js
+++ speech-app/components/Activities.js
@@ -2,16 +2,40 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React, { useState } from 'react';
 import { Pressable } from 'react-native';
+import { TextInput, Button } from 'react-native';
 
 function Activities () {
+    const [text, setText] = useState('');
+
+    const sendActivity = ()=> {
+        console.log('Here');
+    }
 
     return (
         <View>
-            <TextInput>
+            <TextInput
+                style={styles.input}
+                onChangeText={(text) => setText(text)}
+                placeholder='Activity' 
+                value={text}
+            ></TextInput>
+            
+            <Button
+                onPress={sendActivity}
+                title="Send Task"
+            />
 
-            </TextInput>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+});
 
 export default Activities
