f1b4b3839c11ce37a04ab754926447befa27b201
Eduardo <hurtado.josegomez@gmail.com>
2024-02-03 07:35:01 -0600

    script audio

--- backend/scripts/script.js
+++ backend/scripts/script.js
@@ -1,34 +1,61 @@
-document.addEventListener('DOMContentLoaded', () => {
-	myFunc();
+document
+  .getElementById("startRecording")
+  .addEventListener("click", initFunction);
 
-});
+let isRecording = document.getElementById("isRecording");
 
-async function myFunc() {
-	console.log('working');
+function initFunction() {
 
-    const buttonStart = document.querySelector('#buttonStart')
-    const buttonStop = document.querySelector('#buttonStop')
-    const audio = document.querySelector('#audio')
+    // Display recording
+    async function getUserMedia(constraints) {
+        if (window.navigator.mediaDevices) {
+            return window.navigator.mediaDevices.getUserMedia(constraints);
+        }
 
+        let legacyApi =
+        navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia;
 
-    buttonStart.addEventListener((event) => {
-        alert("Start pressed!");
+        if (legacyApi) {
+            return new Promise(function (resolve, reject) {
+                legacyApi.bind(window.navigator)(constraints, resolve, reject);
+            });
+        } else {
+            alert("user api not supported");
+        }
+    }
+
+    isRecording.textContent = "Recording...";
+
+    let audioChunks = [];
+    let rec;
+
+    function handlerFunction(stream) {
+        rec = new MediaRecorder(stream);
+        rec.start();
+        rec.ondataavailable = (e) => {
+        audioChunks.push(e.data);
+        if (rec.state == "inactive") {
+            let blob = new Blob(audioChunks, { type: "audio/mp3" });
+            console.log(blob);
+            document.getElementById("audioElement").src = URL.createObjectURL(blob);
+            }
+        };
+    }
+
+    function startusingBrowserMicrophone(boolean) {
+        getUserMedia({ audio: boolean }).then((stream) => {
+        handlerFunction(stream);
+        });
+    }
+
+    startusingBrowserMicrophone(true);
+
+    // Stoping handler
+    document.getElementById("stopRecording").addEventListener("click", (e) => {
+        rec.stop();
+        isRecording.textContent = "Click play button to start listening";
     });
-
-    buttonStop.addEventListener((event) => {
-        alert("Stop pressed!");
-    });
-
-
-    const stream = await navigator.mediaDevices.getUserMedia({ // <1>
-        video: false,
-        audio: true,
-    });
-
-
-
-
-
-	console.log('here');
-
 }
