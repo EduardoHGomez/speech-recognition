901c65482d52025be05455b592cd1eda92358a0c
Eduardo <hurtado.josegomez@gmail.com>
2024-02-02 23:57:34 -0600

    x

--- backend/scripts/script copy.js
+++ backend/scripts/script copy.js
@@ -10,10 +10,24 @@ async function myFunc() {
     const buttonStop = document.querySelector('#buttonStop')
     const audio = document.querySelector('#audio')
 
+
+    buttonStart.addEventListener((event) => {
+        alert("Start pressed!");
+    });
+
+    buttonStart.addEventListener((event) => {
+        alert("Start pressed!");
+    });
+
+
     const stream = await navigator.mediaDevices.getUserMedia({
         video: false,
         audio: true,
     })
+
+
+
+
 
 	console.log('here');
 
